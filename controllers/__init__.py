dba2a5c040ce4e26449fea4f563598f1bac43325
Llerenas <soporte@terravital.com.mx>
2025-10-03 13:22:41 -0700

    Delete controllers directory

--- controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-controllers = Blueprint('controllers', __name__)
-
-# register all sub-modules so their routes hook into `controllers`
-from . import calendar_view, highlights, fraction_hunter
